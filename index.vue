f90b1ffd6a81f423e47337e0e1d63b87f4bd1d11
azrillll <142952139+azrillll@users.noreply.github.com>
2024-06-03 10:47:42 +0700

    Add files via upload

--- index.vue
+++ index.vue
@@ -1,77 +1,36 @@
 <template>
-    <div class="container-fluid">
-      <div class="row my-5">
-        <div class="col-lg-6">
-          <nuxt-link to="../pengujung/tambah">
-            <div class="card bg-pengunjung rounded-5">
-              <div class="card-body">
-                <h2>Pengunjung</h2>
-              </div>
-            </div>
-          </nuxt-link>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
         </div>
-  
-        <div class="col-lg-6">
-          <nuxt-link to="/buku">
-            <div class="card bg-buku rounded-5">
-              <div class="card-body">
-                <h2>Cari Buku</h2>
-              </div>
-            </div>
-          </nuxt-link>
-        </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>azril</td>
+              <td>siswa</td>
+              <td>24 Februari 2024, 16.31.00</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-    <div class="statistik">
-      <h1>STATISTIK</h1>
-    </div>
-
-    <div class="container-fluid">
-    <div class="row my-5 d-flex justify-content-around-5 ">
-      <div class="col-lg-6 ">
-          <div class="card rounded-5 color1">
-            <div class="card-body d-flex justify-content-around d-flex align-items-center">
-              <h2 class="text"> 3 pengunjung</h2>
-              </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-          
-            <div class="card rounded-5 color2">
-              <div class="card-body d-flex justify-content-around d-flex align-items-center">
-                <h2 class="text">140 Buku</h2>
-              </div>
-            </div>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  
-  <style scoped>
-   .color1{
-    background-color:#13d664;
-  }
-  .color2{
-    background-color: #E2DDFF;
-  }
-  .text{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .card {
-    height: 250px;
-    box-shadow: 1px 1px 10px #424242;
-  }
-  .card.bg-pengunjung {
-    background-image: url(../assets/img/bg-home-kunjungan.jpeg);
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-  }
-  .card.bg-buku {
-    background: url(../assets/img/bg-home-cari-buku.jpg) no-repeat center center;
-    background-size: cover;
-  }
-  </style>
+    <nuxt-link to ="/">
+      <button class="btn btn-primary" type="submit">kembali</button>
+    </nuxt-link>
+  </div>
+</template>
--- pages/pengujung/index.vue
+++ pages/pengujung/index.vue
@@ -1,36 +1,52 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-12">
-        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
-        <div class="my-3">
-          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
-        </div>
-        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
-        <table class="table">
-          <thead>
-            <tr>
-              <td>#</td>
-              <td>NAMA</td>
-              <td>KEANGGOTAAN</td>
-              <td>WAKTU</td>
-              <td>KEPERLUAN</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1.</td>
-              <td>azril</td>
-              <td>siswa</td>
-              <td>24 Februari 2024, 16.31.00</td>
-              <td>Baca</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="row">
+          <div class="col-lg-12">
+              <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+              <nuxt-link to="../"><button type="submit" class="btn btn-lg rounded-5 px-5 bg-primary text-white" style="float: right; margin-bottom: 15px;">KEMBALI</button></nuxt-link>
+              <div class="my-3">
+                  <form @submit.prevent="getpengunjung">
+                      <input v-model="keyword" type="search" class="form-control rounded-5"
+                          placeholder="filter...">
+                  </form>
+              </div>
+              <div class="my-3 text-muted">menampilkan 8 dari 24</div>
+              <table class="table">
+                  <thead>
+                      <tr>
+                          <td>#</td>
+                          <td>NAMA</td>
+                          <td>KEANGGOTAAN</td>
+                          <td>WAKTU</td>
+                          <td>KEPERLUAN</td>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="(visitor, i) in visitors" :key="i">
+                          <td>{{ i+1 }}</td>
+                          <td>{{ visitor.nama }}</td>
+                          <td>{{ visitor.keanggotaan.nama }}</td>
+                          <td>{{ visitor.tanggal }}, {{ visitor.waktu }}</td>
+                          <td>{{ visitor.keperluan.nama }}</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
       </div>
-    </div>
-    <nuxt-link to ="/">
-      <button class="btn btn-primary" type="submit">kembali</button>
-    </nuxt-link>
   </div>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const keyword = ref('')
+const visitors = ref ([])
+
+const getpengunjung = async () => {
+  const {data,error} = await supabase.from('pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+      .ilike('nama', `%${keyword.value}%`)
+  if(data) visitors.value = data
+}
+onMounted(() =>{
+  getpengunjung()
+})
+</script>
